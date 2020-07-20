7e9dd7ec6017fff657ab1301adf20617fa11e55d
Avarokin Saini <saini7@purdue.edu>
2020-07-19 19:27:38 -0500

    Settings page.

--- lib/Pages/AccountSettingsPage.dart
+++ lib/Pages/AccountSettingsPage.dart
@@ -17,7 +17,20 @@ import 'package:google_sign_in/google_sign_in.dart';
 class Settings extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
+    return Scaffold(
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Colors.white
+        ),
+        backgroundColor: Colors.lightBlue,
+        title: Text(
+          "Account Settings",
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+        ),
+        centerTitle: true,
+      ),
+      body: SettingsScreen(),
+    );
   }
 }
 
@@ -30,8 +43,118 @@ class SettingsScreen extends StatefulWidget {
 
 
 class SettingsScreenState extends State<SettingsScreen> {
+
+  TextEditingController nicknameTextEditingController;
+  TextEditingController aboutMeTextEditingController;
+
+  SharedPreferences preferences;
+  String id = "";
+  String nickname = "";
+  String aboutMe = "";
+  String photoUrl = "";
+
+  File imageFile;
+  bool isLoading = false;
+
+  @override
+  void initState() {
+    super.initState();
+    readDataFromLocal();
+  }
+
+  void readDataFromLocal() async {
+    preferences = await SharedPreferences.getInstance();
+    id = preferences.get("id");
+    nickname = preferences.get("nickname");
+    aboutMe = preferences.get("aboutMe");
+    photoUrl = preferences.get("photoUrl");
+
+    nicknameTextEditingController = TextEditingController(text: nickname);
+    aboutMeTextEditingController = TextEditingController(text: aboutMe);
+
+    setState(() {});  // Force refresh
+  }
+
+  Future getImage() async {
+    File newImage = await ImagePicker.pickImage(source: ImageSource.gallery);
+    if (newImage != null) {
+      setState(() {
+        this.imageFile = newImage;
+        isLoading = true;
+      });
+    }
+
+    //uploadImageToFierstoreAndStorage();
+  }
+
+
+
   @override
   Widget build(BuildContext context) {
-
+    return Stack (
+      children: <Widget>[
+        SingleChildScrollView(
+          child: Column(
+            children: <Widget>[
+              // Profile image
+              Container(
+                child: Center(
+                  child: Stack(
+                    children: <Widget>[
+                      (imageFile == null) ? (photoUrl != "") ?
+                          Material(
+                            // Display existing image
+                            child: CachedNetworkImage(
+                              placeholder: (context,url) => Container(
+                                child: CircularProgressIndicator(
+                                  strokeWidth: 2,
+                                  valueColor: AlwaysStoppedAnimation<Color>(Colors.lightBlueAccent),
+                                ),
+                                width: 200,
+                                height: 200,
+                                padding: EdgeInsets.all(20),
+                              ),
+                              imageUrl: photoUrl,
+                              width: 200,
+                              height: 200,
+                              fit: BoxFit.cover,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(125)),
+                            clipBehavior: Clip.hardEdge,
+                          ) : Icon(Icons.account_circle, size: 100,color: Colors.grey,)
+                          : Material(
+                        // Display new image
+                        child: Image.file(
+                            imageFile,
+                          width: 200,
+                          height: 200,
+                          fit: BoxFit.cover,
+                        ),
+                        borderRadius: BorderRadius.all(Radius.circular(125)),
+                        clipBehavior: Clip.hardEdge,
+                      ),
+                      IconButton(
+                        icon: Icon(
+                          Icons.camera_alt,
+                          size: 100,
+                          color: Colors.white54.withOpacity(0.3),
+                        ),
+                        onPressed: getImage,
+                        padding: EdgeInsets.all(0),
+                        splashColor: Colors.transparent,
+                        highlightColor: Colors.grey,
+                        iconSize: 200,
+                      ),
+                    ],
+                  ),
+                ),
+                width: double.infinity,
+                margin: EdgeInsets.all(20),
+              )
+            ],
+          ),
+        ),
+      ],
+    );
   }
 }
--- lib/Pages/LoginPage.dart
+++ lib/Pages/LoginPage.dart
@@ -1,5 +1,4 @@
 import 'dart:async';
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
@@ -12,11 +11,45 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
 
+  LoginScreen({Key key}) : super(key : key);  //Constructor
+
   @override
   LoginScreenState createState() => LoginScreenState();
 }
 
 class LoginScreenState extends State<LoginScreen> {
+
+  final GoogleSignIn googleSignIn = new GoogleSignIn();
+  final FirebaseAuth firebaseAuth = FirebaseAuth.instance;
+  SharedPreferences preferences;
+
+  bool isLoggedin = false;
+  bool isLoading = false;
+
+  FirebaseUser currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+
+    isUserLoggedin();
+  }
+
+  void isUserLoggedin() async {
+    this.setState(() {isLoggedin = true;});
+
+    preferences = await SharedPreferences.getInstance();
+    isLoggedin = await googleSignIn.isSignedIn();
+    if (isLoggedin) {
+      Navigator.push(context, MaterialPageRoute(
+          builder: (context) => HomeScreen(
+              currentUserId: preferences.getString("id"))));
+    }
+    
+    this.setState(() {isLoading = false;});
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -39,6 +72,7 @@ class LoginScreenState extends State<LoginScreen> {
               fontFamily: 'Sans-Serif'),
             ),
             GestureDetector(
+              onTap: controlSignIn,
               child: Column(
                 children: <Widget>[
                   Container(
@@ -56,7 +90,7 @@ class LoginScreenState extends State<LoginScreen> {
                 ),
                   Padding(
                     padding: EdgeInsets.all(1),
-                    child: circularProgress(),
+                    child: (isLoading) ? circularProgress() : Container(),
                   ),
                 ],
               ),
@@ -66,5 +100,71 @@ class LoginScreenState extends State<LoginScreen> {
       ),
     );
   }
+
+  Future<Null> controlSignIn() async {
+
+    this.setState(() {
+      isLoading = true;
+    });
+    GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+    final AuthCredential credential = GoogleAuthProvider.getCredential(idToken:
+      googleAuth.idToken, accessToken: googleAuth.accessToken);
+
+    FirebaseUser firebaseUser =
+        (await firebaseAuth.signInWithCredential(credential)).user;
+    if (firebaseUser == null) {
+      Fluttertoast.showToast(msg: "Sign in failed.");
+      this.setState(() {
+        isLoading = false;
+      });
+
+    } else {
+
+      // Check Firestore for user
+      final QuerySnapshot resultQuery = await Firestore.instance
+          .collection("users").where("id", isEqualTo: firebaseUser.uid)
+          .getDocuments();
+
+      final List<DocumentSnapshot> documentSnapshots = resultQuery.documents;
+
+      if (documentSnapshots.length == 0) {
+        // Save new user info in FireStore
+        Firestore.instance.collection("users").document(firebaseUser.uid).setData({
+          "nickname" : firebaseUser.displayName,
+          "photoUrl" : firebaseUser.photoUrl,
+          "id" : firebaseUser.uid,
+          "aboutMe" : "Hello, world!",
+          "createdAt" : DateTime.now().millisecondsSinceEpoch.toString(),
+          "chattingWith" : null,
+        });
+
+        // Write data to local
+        currentUser = firebaseUser;
+        await preferences.setString("id", currentUser.uid);
+        await preferences.setString("nickaname", currentUser.displayName);
+        await preferences.setString("photoUrl", currentUser.photoUrl);
+
+      } else {
+        // Returning user
+        // Write data to local
+        currentUser = firebaseUser;
+        await preferences.setString("id", documentSnapshots[0]["id"]);
+        await preferences.setString("nickaname", documentSnapshots[0]["nickname"]);
+        await preferences.setString("photoUrl", documentSnapshots[0]["photoUrl"]);
+        await preferences.setString("aboutMe", documentSnapshots[0]["aboutMe"]);
+      }
+
+      Fluttertoast.showToast(msg: "Welcome!");
+      this.setState(() {
+        isLoading = false;
+      });
+
+      Navigator.push(context, MaterialPageRoute(
+          builder: (context) => HomeScreen(currentUserId: firebaseUser.uid,)));
+
+
+    }
+  } // end controlSignIn()
 
 }
